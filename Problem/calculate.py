00a892d88c72b5e19e228aed55af16a7c713a3ec
Matthew <bu5119@hanyang.ac.kr>
2025-09-16 02:56:54 +0900

    Change File Name to English

---
+++ Problem/calculate.py
@@ -0,0 +1,25 @@
+1+2
+3/2.4
+3*9
+a=1
+b=2
+a+b
+C = "Python"
+print(C)
+
+d = 3
+if d > 1:
+    print("d is greater than 1")
+    
+for f in [1, 2, 3]:
+    print(f)
+
+i = 0
+while i < 3:
+    i += 1
+    print(i)
+
+def add(e, f):
+    return e + f
+
+add(3, 4)
