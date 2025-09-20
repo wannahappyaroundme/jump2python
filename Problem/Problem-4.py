06baf7252db1161351cea6cfbde0c3c3632dd7a0
Matthew <bu5119@hanyang.ac.kr>
2025-09-21 03:31:07 +0900

    Finish Q3

--- Problem/Problem-4.py
+++ Problem/Problem-4.py
@@ -16,3 +16,11 @@ def avg_numbers(*args):
 
 print(avg_numbers(1, 2))
 print(avg_numbers(1, 2, 3, 4, 5))
+
+# Q3
+
+input1 = input("첫 번째 숫자를 입력하세요: ")
+input2 = input("두 번째 숫자를 입력하세요: ")
+
+total = int(input1) + int(input2)
+print("두 수의 합은 %s입니다." %total)
