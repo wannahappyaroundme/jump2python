4095486344182da61d545a02e17f724ac5c76310
Matthew <bu5119@hanyang.ac.kr>
2025-09-20 23:57:39 +0900

    딕셔너리 사용

--- Problem/DictionaryMessage.py
+++ Problem/DictionaryMessage.py
@@ -15,3 +15,8 @@ print(grade['julliet'])
 d = {'a': 1, 'b': 2}
 print(d['a'])
 print(d['b'])
+
+dic = {'name': 'pey', 'phone': '0119993323', 'birth': '1118'}
+print(dic['name'])
+print(dic['phone'])
+print(dic['birth'])
