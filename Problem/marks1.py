a8ba7b2bd6f64ca97ad64606c19945adcd6c8a93
Matthew <bu5119@hanyang.ac.kr>
2025-09-21 01:51:13 +0900

    Create marks1.py

---
+++ Problem/marks1.py
@@ -0,0 +1,9 @@
+marks = [90, 25, 67, 45, 80]
+
+number = 0
+for i in marks:
+    number = number + 1
+    if i >= 60:
+        print("%d번 학생은 합격입니다." %number)
+    else:
+        print("%d번 학생은 불합격입니다." %number)
