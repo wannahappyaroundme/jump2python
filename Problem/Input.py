aab198cb4e39842b97b6ac3cdf22df1b694816f6
Matthew <bu5119@hanyang.ac.kr>
2025-09-21 03:08:45 +0900

    Create Input.py

---
+++ Problem/Input.py
@@ -0,0 +1,19 @@
+a = input()
+print(a)
+number = input("숫자를 입력하세요: ")
+print(number)
+
+a = 123
+print(a)
+
+a = "Python"
+print(a)
+
+a = [1, 2, 3]
+print(a)
+
+# Print에서는 큰 따옴표로 둘러싸인 문자열은 + 연산과 동일하다
+# 문자열 띄어쓰기는 쉼표로 한다.
+
+for i in range(10):
+    print(i, end =' ')
