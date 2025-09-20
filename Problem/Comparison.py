eea6a62ed8a6800ba66145bc8316e59665dbd99f
Matthew <bu5119@hanyang.ac.kr>
2025-09-21 01:20:03 +0900

    Card if function

--- Problem/Comparison.py
+++ Problem/Comparison.py
@@ -26,3 +26,14 @@ print('a' in ('a', 'b', 'c'))
 
 print('j' not in 'Python')
 
+pocket = ['paper', 'cellphone', 'money']
+
+if 'money' in pocket:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+    
+if 'card' not in pocket:
+    print("걸어가라")
+else:
+    print("버스를 타고 가라")
