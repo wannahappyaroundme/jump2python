0bc09c161f7dd12697ebc2c091d336bde080e9fc
Matthew <bu5119@hanyang.ac.kr>
2025-09-21 01:16:50 +0900

    OR Comparison

--- Problem/Comparison.py
+++ Problem/Comparison.py
@@ -10,3 +10,12 @@ if money >= 3000:
     print("택시를 타고 가라")
 else:
     print("걸어가라")
+    
+money2 = 2000
+card = True
+
+if money>=3000 or card:
+    print("택시를 타고 가라")
+else:
+    print("걸어가라")
+    
