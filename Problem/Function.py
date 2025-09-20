eb90574cf4254d0b7be98d67fbd41ddf74c202dd
Matthew <bu5119@hanyang.ac.kr>
2025-09-21 02:21:33 +0900

    매개변수 리턴값에 따른 함수의 변화

--- Problem/Function.py
+++ Problem/Function.py
@@ -6,3 +6,27 @@ b = 4
 c = add(a, b)
 
 print(c)
+
+def say():
+    return "Hi"
+    
+d = say()
+print(d)
+
+def add2(a, b):
+    print("%d, %d의 합은 %d입니다." % (a, b, a+b))
+
+print(add2(3,4))
+
+add2(3,4)
+
+def say2():
+    print("Hi")
+    
+say2()
+
+def sub(a, b):
+    return a - b
+
+result = sub(a=7, b=3)
+print(result)
