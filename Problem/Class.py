915f8a5101e95abd9a80b6a6d17c06ce5c9b65c7
Matthew <bu5119@hanyang.ac.kr>
2025-09-22 00:46:29 +0900

    Add b.setdata in Class.py

--- Problem/Class.py
+++ Problem/Class.py
@@ -2,9 +2,35 @@ class FourCal:
     def setdata(self, first, second):
         self.first = first
         self.second = second
-
+    
+    def add(self):
+        result = self.first + self.second
+        return result
+        
+    def mul(self):
+        result = self.first * self.second
+        return result
+        
+    def sub(self):
+        result = self.first - self.second
+        return result
+        
+    def div(self):
+        result = self.first / self.second
+        return result
+    
 a = FourCal()
 print(type(a))
-a.setdata(4,2)
+a.setdata(4, 2)
+b = FourCal()
+b.setdata(3, 8)
 print(a.first)
 print(a.second)
+print(a.add())
+print(a.mul())
+print(a.sub())
+print(a.div())
+print(b.add())
+print(b.mul())
+print(b.sub())
+print(b.div())
