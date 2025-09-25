a1762f7b5c2663d28b993d070bf0196cc7abbdad
Matthew <bu5119@hanyang.ac.kr>
2025-09-26 06:34:07 +0900

    Create math_test

---
+++ Problem/math_test.py
@@ -0,0 +1,9 @@
+import math
+import random
+
+print(math.gcd(60, 100, 80))
+print(60/20, 100/20, 80/20)
+print(math.lcm(15, 25))
+print(random.random())
+print(random.randint(1, 10))
+print(random.randint(1, 55))
