844351ba6c2a007468870ec9c4755e1db8698a11
Matthew <bu5119@hanyang.ac.kr>
2025-09-26 06:43:20 +0900

    Create random_pop.py / random_pop2.py

---
+++ Problem/random_pop2.py
@@ -0,0 +1,11 @@
+import random
+
+def random_pop(data):
+    number = random.choice(data)
+    data.remove(number)
+    return number
+
+if __name__ == "__main__":
+    data = [1, 2, 3, 4, 5]
+    while data:
+        print(random_pop(data))
