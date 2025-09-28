692170660025d30ae07932c88a0ed7b9f5ec4d63
Matthew <bu5119@hanyang.ac.kr>
2025-09-28 17:39:07 +0900

    Revise thread_test.py & Add threading

--- Problem/thread_test.py
+++ Problem/thread_test.py
@@ -1,4 +1,5 @@
 import time
+import threading
 
 def long_task():
     for i in range(5):
@@ -7,7 +8,16 @@ def long_task():
         
 print("Start")
 
+treads = []
 for i in range(5):
-    long_task()
+    # long_task()
+    t = threading.Thread(target=long_task)
+    treads.append(t)
 
+for t in treads:
+    t.start()
+
+for t in treads:
+    t.join()
+    
 print("End")
