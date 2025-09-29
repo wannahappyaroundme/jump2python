2f79d9cf2dd125a38c916b4ab8199386fbd58185
Matthew <bu5119@hanyang.ac.kr>
2025-09-30 05:26:56 +0900

    Create gugu.py / Create program_test.py

---
+++ Problem/program_test.py
@@ -0,0 +1,14 @@
+def gugu(n):
+    result = []
+    result.append(n*1)
+    result.append(n*2)
+    result.append(n*3)
+    result.append(n*4)
+    result.append(n*5)
+    result.append(n*6)
+    result.append(n*7)
+    result.append(n*8)
+    result.append(n*9)
+    return result
+
+print(gugu(2))
