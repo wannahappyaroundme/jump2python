75cbfbdc6de4baa751051f650920828ba085b484
Matthew <bu5119@hanyang.ac.kr>
2025-10-02 01:20:32 +0900

    upgrade tabto4.py

--- Problem/tabto4.py
+++ Problem/tabto4.py
@@ -3,5 +3,9 @@ import sys
 src = sys.argv[1]
 dst = sys.argv[2]
 
-print(src)
-print(dst)
+f = open(src)
+tab_content = f.read()
+f.close()
+
+space_content = tab_content.replace('\t', ' '*4)
+print(space_content)
