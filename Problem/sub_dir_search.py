7ff1d27c146df7ca569cc1fae52587e0e3785d24
Matthew <bu5119@hanyang.ac.kr>
2025-10-02 01:46:34 +0900

    finding all files

--- Problem/sub_dir_search.py
+++ Problem/sub_dir_search.py
@@ -1,11 +1,17 @@
 import os
 
 def search(dirname):
-    filename = os.listdir(dirname)
-    for filename in filename:
-        full_filename = os.path.join(dirname, filename)
-        ext = os.path.splitext(full_filename)[-1]
-        if ext == '.py':
-            print(full_filename)
+    try:
+        filename = os.listdir(dirname)
+        for filename in filename:
+            full_filename = os.path.join(dirname, filename)
+            if os.path.isdir(full_filename):
+                search(full_filename)
+            else:
+                ext = os.path.splitext(full_filename)[-1]
+                if ext == '.py':
+                    print(full_filename)
+    except PermissionError:
+        pass
     
 search("/Users")  # Mac
