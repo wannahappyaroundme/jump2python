3c18844727250314a9f4709ab915f1f5dbd0a93a
Matthew <bu5119@hanyang.ac.kr>
2025-10-02 01:45:07 +0900

    finding py files

--- Problem/sub_dir_search.py
+++ Problem/sub_dir_search.py
@@ -1,4 +1,11 @@
+import os
+
 def search(dirname):
-    print(dirname)
+    filename = os.listdir(dirname)
+    for filename in filename:
+        full_filename = os.path.join(dirname, filename)
+        ext = os.path.splitext(full_filename)[-1]
+        if ext == '.py':
+            print(full_filename)
     
-search("c:/")  # Windows
+search("/Users")  # Mac
