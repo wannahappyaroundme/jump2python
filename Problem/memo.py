ac83883b20a4055cb4b88b6c5ebdc9f314e9d897
Matthew <bu5119@hanyang.ac.kr>
2025-10-02 01:00:13 +0900

    Create memo.py

--- Problem/memo.py
+++ Problem/memo.py
@@ -1,7 +1,12 @@
 import sys
 
 option = sys.argv[1]
-memo = sys.argv[2]
+
+if option == '-a':
+    memo = sys.argv[2]
+    f = open('memo.txt', 'a')
+    f.write(memo + '\n')
+    f.close()
 
 print(option)
 print(memo)
