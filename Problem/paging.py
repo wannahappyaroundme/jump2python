b7b4230242a547c82b684cc52aa7b32541e569e1
Matthew <bu5119@hanyang.ac.kr>
2025-10-02 00:56:32 +0900

    upgrade paging.py

--- Problem/paging.py
+++ Problem/paging.py
@@ -1,5 +1,8 @@
 def get_total_page(m, n):
-    return m // n + 1
+    if m % n == 0:
+        return m // n
+    else:
+        return m // n + 1
 
 print(get_total_page(5, 10))
 print(get_total_page(15, 10))
