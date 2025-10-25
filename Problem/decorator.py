7f03f29db3695f53d02fd42c88431043683041f9
Matthew <bu5119@hanyang.ac.kr>
2025-10-25 18:47:29 +0900

    Create decorator.py

---
+++ Problem/decorator.py
@@ -0,0 +1,16 @@
+import time
+
+def elapsed(original_func):
+    def wrapper():
+        start = time.time()
+        result = original_func()
+        end = time.time()
+        print("함수 수행 시간 : %f 초" % (end - start))
+        return result
+    return wrapper
+     
+def myfunc():
+    print("함수가 실행됩니다.")
+    
+decorated_myfunc = elapsed(myfunc)
+decorated_myfunc()
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 교재의 예제 문제 풀이 및 강사의 정답 코드
 
 ## Life is too short, You need Python
+
 ### 인생은 너무 짧으니 파이썬이 필요해.
 
 ### 백문이 불어일견, 백견이 불어일타
+
+# Visual computing lab 가보자곰 화이팅@!
