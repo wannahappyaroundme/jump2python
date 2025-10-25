e2bd0b1bd33bdd40c33e7b150f4b781e55514089
Matthew <bu5119@hanyang.ac.kr>
2025-10-25 18:53:39 +0900

    Create decorator2.py (Adding variable in decorator function)

---
+++ Problem/decorator2.py
@@ -0,0 +1,19 @@
+import time
+
+def elapsed(original_func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = original_func(*args, **kwargs)
+        end = time.time()
+        print("함수 수행 시간 : %f 초" % (end - start))
+        return result
+    return wrapper
+     
+@elapsed
+def myfunc(msg):
+    print("'%s'을 출력합니다." % msg)    
+
+# decorated_myfunc = elapsed(myfunc)
+# decorated_myfunc()
+
+myfunc("You need python")
