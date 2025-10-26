9f336d953609dc0b6cac5997209818a0a991eef9
Matthew <bu5119@hanyang.ac.kr>
2025-10-26 23:09:16 +0900

    Create totalexam.py

--- Problem/totalexam.py
+++ Problem/totalexam.py
@@ -1,103 +1,130 @@
-# 1
-# a:b:c:d -> a#b#c#d
+# # 1
+# # a:b:c:d -> a#b#c#d
 
-text = "a:b:c:d"
-A = text.split(":")
-answer = "#".join(A)
+# text = "a:b:c:d"
+# A = text.split(":")
+# answer = "#".join(A)
 
-print(answer)
+# print(answer)
 
-# 2
-# key에 해당하는 value를 출력하는 프로그램
+# # 2
+# # key에 해당하는 value를 출력하는 프로그램
 
-a = {'A':90, 'B':80}
-print(a.get('C', 70))
+# a = {'A':90, 'B':80}
+# print(a.get('C', 70))
 
-# 3
-# Extend와 + [4, 5]의 차이
+# # 3
+# # Extend와 + [4, 5]의 차이
 
-a = [1, 2, 3]
-a = a + [4, 5]
-a.append([4, 5])
+# a = [1, 2, 3]
+# a = a + [4, 5]
+# a.append([4, 5])
 
-# extend() = 원본 수정, +는 새 리스트 생성
+# # extend() = 원본 수정, +는 새 리스트 생성
 
-# 4
-# A학급 학생의 점수를 나타내는 리시트 다음 리스트에서 50점 이상 점수의 총합 
+# # 4
+# # A학급 학생의 점수를 나타내는 리시트 다음 리스트에서 50점 이상 점수의 총합 
 
-A = [20, 55, 67, 82, 45, 33, 90, 87, 100, 25]
-result = 0
+# A = [20, 55, 67, 82, 45, 33, 90, 87, 100, 25]
+# result = 0
 
-for i in A:
-    if i >= 50:
-        result += i
+# for i in A:
+#     if i >= 50:
+#         result += i
     
-print(result)
+# print(result)
 
-# 5
-# 피보나치 수열 만들기
+# # 5
+# # 피보나치 수열 만들기
 
-def fibo(n):
-    if n <= 0:
-        return []
-    elif n == 1:
-        return [0]
-    elif n == 2:
-        return [0, 1]
-    result = [0, 1]
-    for i in range(n-2):
-        next_num = result[-1]+result[-2]
-        result.append(next_num)
-    return result
+# def fibo(n):
+#     if n <= 0:
+#         return []
+#     elif n == 1:
+#         return [0]
+#     elif n == 2:
+#         return [0, 1]
+#     result = [0, 1]
+#     for i in range(n-2):
+#         next_num = result[-1]+result[-2]
+#         result.append(next_num)
+#     return result
     
-n = int(input("몇 항까지 출력할까요?"))
-print(fibo(n))
+# n = int(input("몇 항까지 출력할까요?"))
+# print(fibo(n))
 
-# 6 
-# 숫자 총합 구하기
+# # 6 
+# # 숫자 총합 구하기
 
-number = input("입력시킬 숫자를 적어주세요 :")
-num2 = number.split(",")
-sum = 0
-for i in num2:
-    sum += int(i)
+# number = input("입력시킬 숫자를 적어주세요 :")
+# num2 = number.split(",")
+# sum = 0
+# for i in num2:
+#     sum += int(i)
     
-print(sum)
+# print(sum)
 
-# 7
-# 한 줄 구구단
+# # 7
+# # 한 줄 구구단
 
-gugudan = int(input("구구단을 출력할 숫자를 입력하세요(2~9):"))
-for i in range(1,10):
-    dan = i * gugudan
-    print(dan, end=" ")
-print()
+# gugudan = int(input("구구단을 출력할 숫자를 입력하세요(2~9):"))
+# for i in range(1,10):
+#     dan = i * gugudan
+#     print(dan, end=" ")
+# print()
 
-# 8
-# 파일 읽어서 역순으로 정하기
-f = open('/Users/kyungsbook/Desktop/jump2python/Problem/abc.txt', 'r')
-lines = f.readlines()
-f.close()
+# # 8
+# # 파일 읽어서 역순으로 정하기
+# f = open('/Users/kyungsbook/Desktop/jump2python/Problem/abc.txt', 'r')
+# lines = f.readlines()
+# f.close()
 
-lines.reverse()
+# lines.reverse()
 
-f = open('/Users/kyungsbook/Desktop/jump2python/Problem/abc.txt', 'w')
-for line in lines:
-    line = line.strip()
-    f.write(line)
-    f.write("\n")
-f.close()
+# f = open('/Users/kyungsbook/Desktop/jump2python/Problem/abc.txt', 'w')
+# for line in lines:
+#     line = line.strip()
+#     f.write(line)
+#     f.write("\n")
+# f.close()
 
-# 9
-f = open('/Users/kyungsbook/Desktop/jump2python/Problem/sample.txt', 'r')
-lines = f.readlines()
-f.close()
+# # 9
+# f = open('/Users/kyungsbook/Desktop/jump2python/Problem/sample.txt', 'r')
+# lines = f.readlines()
+# f.close()
 
-total = 0
-for line in lines:
-    total += int(line)
-average = total /len(lines)
+# total = 0
+# for line in lines:
+#     total += int(line)
+# average = total /len(lines)
 
-f = open('/Users/kyungsbook/Desktop/jump2python/Problem/result.txt', "w")
-f.write(str(average))
-f.close()
+# f = open('/Users/kyungsbook/Desktop/jump2python/Problem/result.txt', "w")
+# f.write(str(average))
+# f.close()
+
+# 10
+# 계산기 만들기
+
+class Calculator:
+    def __init__(self, data):
+        self.data = data
+    
+    def sum(self):
+        result = 0
+        for d in self.data:
+            result += d
+        return result
+    
+    def avg(self):
+        average = self.sum() / len(self.data)
+        return average
+
+cal1 = Calculator([1, 2, 3, 4, 5])
+print(cal1.sum())
+print(cal1.avg())
+
+cal2 = Calculator([6, 7, 8, 9, 10])
+print(cal2.sum())
+print(cal2.avg())        
+        
+        
